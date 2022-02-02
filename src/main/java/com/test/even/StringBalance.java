d550f350cd33ed64f3d0d0ad48ba865d4b974f61
Sumith <sumith_mathew@ymail.com>
2022-02-01 22:05:01 -0500

    Initial Commit

---
+++ src/main/java/com/test/even/StringBalance.java
@@ -0,0 +1,20 @@
+package com.test.even;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *  Created by: payaras User: sumith.payaranalmathew@dominos.com Date: 02/01/22.
+ *  Write a function that is given a string and returns a boolean indicating whether that string has balanced parentheses.
+ *  The string /may have characters other than parentheses, and should be ignored. e.g. ‘x()’ is balanced,‘((y(z‘ is not, ‘a(b())’ is balanced, ‘)()(’ is not.
+ */
+
+public class StringBalance {
+
+    public boolean isBalanced(String value){
+        //Write your code here
+
+        return false;
+    }
+
+}
---
+++ src/test/java/com/test/even/StringBalanceTest.java
@@ -0,0 +1,34 @@
+package com.test.even;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringBalanceTest {
+
+    private StringBalance balance = new StringBalance();
+
+    @Test
+    @DisplayName("Validate is not null")
+    void shouldNotBeNull() {
+        String value = null;
+        assertFalse(balance.isBalanced(value), "should not be null");
+    }
+
+    @Test
+    @DisplayName("Check string is balanced")
+    void shouldbalanceSimpleString() {
+        String value = "ab(c)(d())";
+        assertTrue(balance.isBalanced(value), "string is balanced");
+    }
+
+    @Test
+    @DisplayName("String is not balanced")
+    void shouldCompressCombinationString() {
+        String value = "((x()())";
+        assertFalse(balance.isBalanced(value), "string is not balanced");
+    }
+
+
+}
